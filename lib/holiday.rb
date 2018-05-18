b455d3560032304ff959301e08ba2823bf64d080
taryn yaeger <tsyaeger@gmail.com>
2018-05-18 07:10:50 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -72,7 +72,7 @@ def all_supplies_in_holidays(holiday_hash)
   
   holiday_hash.each do |season, details|
     season = "#{season.capitalize}:"
-	 puts season
+	  puts season
    
    details.each do |attribute, data|
 
