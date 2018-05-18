706f450b525696be26d4e34cc6d5dc0b57431992
taryn yaeger <tsyaeger@gmail.com>
2018-05-18 07:15:32 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -98,7 +98,7 @@ def all_holidays_with_bbq(holiday_hash)
   # holiday_hash.each do |season, details|
   #   details.each do |attribute, data|
   #     puts true
-
+  bbqs = []
   holiday_hash.each do |season, details|
     details.each do |attribute, data|
       if data.include?("BBQ")
